e9af086e6f641ae75d8f6c74e8bb741d993761dc
Asileka <asilkatya@gmail.com>
2023-05-23 23:12:03 +0100

    backup commit

---
+++ indigo.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const csvFilePath = "./ShippiRawData.csv";
+const csv = require("csvtojson");
+const sampleReqObj = {
+  shipWeight: "1",
+  shipType: "International",
+  shipSendAddrs: "BL6A76",
+  shipRecieveAddrs: "M73RT",
+  shipDate: "26/04/2024",
+};
+csv()
+  .fromFile(csvFilePath)
+  .then((jsonObj) => {
+    console.log("csv converted");
+  });
+app.use(bodyParser.json());
+app.get("/api/helloworld", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/api/jsonobj", async (req, res) => {
+  const jsonObj = await csv().fromFile(csvFilePath);
+  res.send(jsonObj[0]["Max Shipment Weight (KG)"]);
+});
+app.post("/api/shipmentinfo", async (req, res) => {
+  const jsonObj = await csv().fromFile(csvFilePath);
+  const result = jsonObj.filter(
+    (shipment) =>
+      shipment["Min Shipment Weight (KG)"] <= sampleReqObj.shipWeight &&
+      shipment["Max Shipment Weight (KG)"] >= sampleReqObj.shipWeight
+  );
+  console.log(result);
+  res.json(result[0]);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
